0679942c6fe62d750a6160513c4100633ef2c02b
윤수영 <yunsuyeong@yunsuyeong-ui-MacBookAir.local>
2023-02-02 17:14:51 +0900

    2023/02/02(Blog Page)

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,7 +10,18 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-mdx`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        // The unique name for each instance
+        name: `pages`,
+        // Path to the directory
+        path: `${__dirname}/blog-posts`,
+      },
+    },
+  ],
 };
 
 export default config;
--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,13 +1,42 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Seo from "../components/Seo";
+import { graphql, PageProps } from "gatsby";
 
-export default function Blog() {
+export default function Blog({ data }: PageProps<Queries.BlogPostsQuery>) {
   return (
-    <Layout title="Hello welcome to my blog!">
-      <p>The most recent news from my shop.</p>
+    <Layout title="Blog">
+      <section>
+        {data.allMdx.nodes.map((file, index) => (
+          <article key={index}>
+            <h3>{file.frontmatter?.title}</h3>
+            <h5>
+              {file.frontmatter?.author} in: {file.frontmatter?.category}
+            </h5>
+            <h6>{file.frontmatter?.date}</h6>
+            <hr />
+            <p>{file.excerpt}</p>
+          </article>
+        ))}
+      </section>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query BlogPosts {
+    allMdx {
+      nodes {
+        frontmatter {
+          author
+          category
+          date(formatString: "YYYY.MM.DD")
+          title
+        }
+        excerpt(pruneLength: 100)
+      }
+    }
+  }
+`;
 
 export const Head = () => <Seo title="Blog" />;
